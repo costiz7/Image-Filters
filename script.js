f13b9a66a8de023069d720442f95bd4777290541
costiz7 <costelzanescu7@gmail.com>
2025-11-30 13:18:43 +0200

    Added elements for the app

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//GitHub button
 document.querySelector('.github').addEventListener('click', () => {
     window.location.href = 'https://github.com/costiz7/Image-Filters';
 });
 
+//LinkedIn button
 document.querySelector('.linkedin').addEventListener('click', () => {
     window.location.href = 'https://www.linkedin.com/in/costelz7/';
 });
