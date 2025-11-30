8a47be2215b56c2dac2863790954c076d2bb3559
costiz7 <costelzanescu7@gmail.com>
2025-11-30 19:02:31 +0200

    This is my design

--- script.js
+++ script.js
@@ -7,3 +7,44 @@ document.querySelector('.github').addEventListener('click', () => {
 document.querySelector('.linkedin').addEventListener('click', () => {
     window.location.href = 'https://www.linkedin.com/in/costelz7/';
 });
+
+const fileImport = document.getElementById('fileInput');
+const btnImport = document.getElementById('btn-import');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+btnImport.addEventListener('click', () => {
+    fileImport.click();
+});
+
+fileImport.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+
+    if(!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+        const img = new Image();
+
+        img.onload = () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+
+            context.drawImage(img, 0, 0);
+
+            originalImageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+            enableButtons();
+        };
+
+        img.src = event.target.result;
+    };
+
+    reader.readAsDataURL(file);
+});
+
+function enableButtons() {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(btn => btn.disabled = false);
+}
